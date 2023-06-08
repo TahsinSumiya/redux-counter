2a16ef183551549e123d02f2449d0562e7fb7f78
Tahsin Sumiya <Tahsinadity187@gmail.com>
2023-06-08 18:34:41 +0600

    first commit

---
+++ src/features/Counter/Coin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Coin() {
+  return (
+    <div>
+       <span className='value'>Coin : 0</span>
+    </div>
+  )
+}
---
+++ src/features/Counter/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react';
+export default function Counter() {
+  return (
+    <div>
+      <div className='app'>
+ <button className='button btn1' onClick={()=>{}} aria-label='increment'>+</button>
+ <span className='value'>Count : 0</span>
+ <button className='button btn2' onClick={()=>{}} aria-label='decrement'>-</button>
+    </div>
+    </div>
+  )
+}
